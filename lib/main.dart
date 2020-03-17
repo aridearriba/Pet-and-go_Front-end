ed221aeb3bf810f2375a9d2d71c149cd34af75a9
Ari de Arriba <aridease@gmail.com>
2020-03-17 20:27:22 +0100

    Email and password fields with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-    // This widget is the root of your application.
+    static const String _title = 'Pet and Go';
+
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData(
-                // This is the theme of your application.
-                //
-                // Try running your application with "flutter run". You'll see the
-                // application has a blue toolbar. Then, without quitting the app, try
-                // changing the primarySwatch below to Colors.green and then invoke
-                // "hot reload" (press "r" in the console where you ran "flutter run",
-                // or simply save your changes to "hot reload" in a Flutter IDE).
-                // Notice that the counter didn't reset back to zero; the application
-                // is not restarted.
-                primarySwatch: Colors.blue,
+            title: _title,
+            home: Scaffold(
+                appBar: AppBar(title: const Text(_title)),
+                body: MyStatefulWidget(),
             ),
-            home: MyHomePage(title: 'Flutter Demo Home Page'),
         );
     }
 }
 
-class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
-
-    // This widget is the home page of your application. It is stateful, meaning
-    // that it has a State object (defined below) that contains fields that affect
-    // how it looks.
-
-    // This class is the configuration for the state. It holds the values (in this
-    // case the title) provided by the parent (in this case the App widget) and
-    // used by the build method of the State. Fields in a Widget subclass are
-    // always marked "final".
-
-    final String title;
+class MyStatefulWidget extends StatefulWidget {
+    MyStatefulWidget({Key key}) : super(key: key);
 
     @override
-    _MyHomePageState createState() => _MyHomePageState();
+    _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-    int _counter = 0;
-
-    void _incrementCounter() {
-        setState(() {
-            // This call to setState tells the Flutter framework that something has
-            // changed in this State, which causes it to rerun the build method below
-            // so that the display can reflect the updated values. If we changed
-            // _counter without calling setState(), then the build method would not be
-            // called again, and so nothing would appear to happen.
-            _counter++;
-        });
-    }
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+    final _formKey = GlobalKey<FormState>();
+    var _password = 'hola';
 
     @override
     Widget build(BuildContext context) {
-        // This method is rerun every time setState is called, for instance as done
-        // by the _incrementCounter method above.
-        //
-        // The Flutter framework has been optimized to make rerunning build methods
-        // fast, so that you can just rebuild anything that needs updating rather
-        // than having to individually change instances of widgets.
-        return Scaffold(
-            appBar: AppBar(
-                // Here we take the value from the MyHomePage object that was created by
-                // the App.build method, and use it to set our appbar title.
-                title: Text(widget.title),
-            ),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Text(
-                            'Email',
-                        ),
-                        TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Escribe tu email'
+        return Form(
+            key: _formKey,
+            child: Column(
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Email',
                             ),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                                RegExp regex = new RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+                                if (value.isEmpty) {
+                                    return 'Por favor, escribe un email';
+                                }
+                                else if (!regex.hasMatch(value)) {
+                                    return 'Escribe un email válido';
+                                }
+                                return null;
+                            },
                         ),
-                    ],
-                ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Contraseña',
+                            ),
+                            validator: (value) {
+                                if (value.isEmpty) {
+                                    return 'Por favor, escribe tu constraseña';
+                                }
+                                else if (value != _password) {
+                                    return 'Contraseña incorrecta';
+                                }
+                                return null;
+                            },
+                            onSaved: (value) => _password = value,
+                            obscureText: true,
+                        ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 30.0),
+                        child: RaisedButton(
+                            onPressed: () {
+                                // Validate will return true if the form is valid, or false if
+                                // the form is invalid.
+                                if (_formKey.currentState.validate()) {
+                                    // Process data.
+                                }
+                            },
+                            child: Text('Log in'),
+                        ),
+                    ),
+                ],
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
         );
     }
 }
