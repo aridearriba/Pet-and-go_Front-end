3620795074f9bb3ca06e39ec08cff363bebd419b
Miguel <miguel-s-g@hotmail.com>
2020-03-20 13:09:45 +0100

    ProfilePage icono de usuario + datos personales

--- lib/profile.dart
+++ lib/profile.dart
@@ -11,6 +11,11 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
 
+    String nick = 'UserName';
+    String name = 'usuario';
+    String surname = 'de ejemplo';
+    String email = 'user@email.com';
+
     nLogIn() {
         Navigator.push(
             context,
@@ -29,20 +34,93 @@ class _ProfileState extends State<Profile> {
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
-                title: Text('Pet & Go'),
+                title: Text(
+                    'Pet & Go',
+                    style: TextStyle(
+                        color: Colors.white,
+                    ),
+                ),
                 actions: <Widget>[
-                    IconButton(icon : Icon(Icons.dehaze), color: Colors.white, onPressed: () {},),
+                    IconButton(icon : Icon(Icons.settings), color: Colors.white, onPressed: () {},),
                 ],
             ),
-            body: Center(
-                child: Text(
-                    'PRERFIL'
-                ),
+
+            body: Column(
+                children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                        child: Row(
+                            children: <Widget>[
+                                Icon(
+                                    Icons.account_circle,
+                                    color: Colors.green,
+                                    size: 150.0,
+                                ),
+                                Column(
+                                    children: <Widget>[
+                                        Text(
+                                            '$nick',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 30.0,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                        ),
+
+                                        Text(
+                                            '$name $surname',
+                                            style: TextStyle(
+                                                color: Colors.grey[600],
+                                                fontStyle: FontStyle.italic,
+                                                fontSize: 20.0,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                        ),
+
+                                        Row(
+                                            children: <Widget>[
+                                                Icon(
+                                                    Icons.email,
+                                                    color: Colors.grey[600],
+                                                ),
+                                                Text(
+                                                    '   $email',
+                                                    style: TextStyle(
+                                                        color: Colors.blueAccent,
+                                                    ),
+                                                )
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
+                    ),
+
+                    Padding(
+                        padding:  const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: Row(
+                            children: <Widget>[
+                                Icon(
+                                    Icons.email,
+                                    color: Colors.grey[600],
+                                ),
+                                Text(
+                                    '   $email',
+                                    style: TextStyle(
+                                        color: Colors.blueAccent,
+                                    ),
+                                )
+                            ],
+                        ),
+                    ),
+                ]
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: nLogIn,
-                backgroundColor: Colors.green,
-                child: Text('LogOut'),
+                floatingActionButton: FloatingActionButton(
+                    onPressed: nLogIn,
+                    backgroundColor: Colors.green,
+                    child: Text('LogOut'),
             ),
         );
     }
