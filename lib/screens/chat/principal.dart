dde3ef5cc71cff9df7de29caf4e17429d2137c6f
Coral <cvila98@gmail.com>
2020-05-09 22:20:28 +0200

    llista de chats

--- lib/screens/chat/principal.dart
+++ lib/screens/chat/principal.dart
@@ -28,6 +28,128 @@ class _PrincipalState extends State<Principal>{
                     color: Colors.white,
                 ),
             ),
+            body: Column(
+                children: <Widget>[
+                    RecentChats()
+                ],
+            )
         );
   }
+}
+
+class RecentChats extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+        return Expanded(
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0),
+                    ),
+                ),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0),
+                    ),
+                    child: ListView.builder(
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                                onTap: () => {
+                                },
+                                child: Container(
+                                    margin: EdgeInsets.only(top: 5.0, bottom: 5.0, right: 20.0),
+                                    padding:
+                                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                                    /*decoration: BoxDecoration(
+                                        color: chat.unread ? Color(0xFFFFEFEE) : Colors.white,
+                                        borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(20.0),
+                                            bottomRight: Radius.circular(20.0),
+                                        ),
+                                    ),*/
+                                    child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                            Row(
+                                                children: <Widget>[
+                                                    CircleAvatar(
+                                                        radius: 35.0,
+                                                        child: Icon(Icons.person),
+                                                    ),
+                                                    SizedBox(width: 10.0),
+                                                    Column(
+                                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                                        children: <Widget>[
+                                                            Text(
+                                                                'Usuario1',
+                                                                style: TextStyle(
+                                                                    color: Colors.grey,
+                                                                    fontSize: 15.0,
+                                                                    fontWeight: FontWeight.bold,
+                                                                ),
+                                                            ),
+                                                            SizedBox(height: 5.0),
+                                                            Container(
+                                                                width: MediaQuery.of(context).size.width * 0.45,
+                                                                child: Text(
+                                                                    'Mensaje de prueba',
+                                                                    style: TextStyle(
+                                                                        color: Colors.blueGrey,
+                                                                        fontSize: 15.0,
+                                                                        fontWeight: FontWeight.w600,
+                                                                    ),
+                                                                    overflow: TextOverflow.ellipsis,
+                                                                ),
+                                                            ),
+                                                        ],
+                                                    ),
+                                                ],
+                                            ),
+                                            Column(
+                                                children: <Widget>[
+                                                    Text(
+                                                        '17:06',
+                                                        style: TextStyle(
+                                                            color: Colors.grey,
+                                                            fontSize: 15.0,
+                                                            fontWeight: FontWeight.bold,
+                                                        ),
+                                                    ),
+                                                    SizedBox(height: 5.0),
+                                                    /*chat.unread
+                                                        ? */Container(
+                                                        width: 40.0,
+                                                        height: 20.0,
+                                                        decoration: BoxDecoration(
+                                                            color: Theme.of(context).primaryColor,
+                                                            borderRadius: BorderRadius.circular(30.0),
+                                                        ),
+                                                        alignment: Alignment.center,
+                                                        child: Text(
+                                                            'NEW',
+                                                            style: TextStyle(
+                                                                color: Colors.white,
+                                                                fontSize: 12.0,
+                                                                fontWeight: FontWeight.bold,
+                                                            ),
+                                                        ),
+                                                    )
+                                                        /*: Text(''),*/
+                                                ],
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                            );
+                        },
+                    ),
+                ),
+            ),
+        );
+    }
+
 }
