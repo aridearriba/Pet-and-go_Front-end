99aa39a9f74c816c5e8ac9dedb34f5bc30d216e2
lbelloso <luisbelloso87@gmail.com>
2020-05-11 01:30:27 +0200

    add imagenes google streetViews

--- lib/screens/quedadas/SquarePerreParadaView.dart
+++ lib/screens/quedadas/SquarePerreParadaView.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'dart:typed_data';
-
+import 'package:petandgo/global/global.dart' as Global;
 import 'package:flutter/material.dart';
 import 'package:petandgo/model/PerreParada.dart';
 import 'package:petandgo/model/user.dart';
 import 'package:petandgo/screens/quedadas/vistaPerreParada.dart';
+import '../../Credentials.dart';
+import 'package:http/http.dart' as http;
 import 'perreParadaTabView.dart';
 
 class SquarePerreParadaWidget extends StatefulWidget {
@@ -32,45 +34,87 @@ class SquarePerreParadaView extends State<SquarePerreParadaWidget> {
     Widget build(BuildContext context) {
         return GestureDetector(
             onTap: () => nPerreParadaView(),
-            child: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: getImage(),
-                        fit: BoxFit.cover,
-                    ),
-                ),
-                height: 200.0,
-                alignment: Alignment.topLeft, // where to position the child
-                child: Container(
-                    height: MediaQuery.of(context).size.height * 0.04,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.lightGreen,
-                    ),
-                    child: Stack(
-                        children: <Widget>[
-                            Icon(
-                                Icons.map,
-                                color: Colors.black,
-                                size: MediaQuery.of(context).size.height * 0.04,
-                            ),
-                            Center(
-                                child: Text(widget.miniQuedada.lugarInicio, textAlign: TextAlign.center,)
-                            ),
-                        ],
-                    ),
-                ),
-            ),
+            child: buildImage(context)
         );
     }
 
-    ImageProvider getImage() {
-        // no image
-        if (widget.miniQuedada.fotoLugar == "")
-            return AssetImage("assets/images/parkDog.jpg");
+    @override
+    Widget buildImage(BuildContext context) {
+        return FutureBuilder(
+            future: getImage(),
+            builder: (BuildContext context, AsyncSnapshot<ImageProvider> snapshot) {
+                if (! snapshot.hasData) {
+                    return Center(
+                        child: CircularProgressIndicator(),
+                    );
+                } else {
+                    return Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: snapshot.data,
+                                fit: BoxFit.cover,
+                            ),
+                        ),
+                        height: 200.0,
+                        alignment: Alignment.topLeft, // where to position the child
+                        child: Container(
+                            height: MediaQuery.of(context).size.height * 0.04,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                color: Colors.lightGreen,
+                            ),
+                            child: Stack(
+                                children: <Widget>[
+                                    Icon(
+                                        Icons.map,
+                                        color: Colors.black,
+                                        size: MediaQuery.of(context).size.height * 0.04,
+                                    ),
+                                    Center(
+                                        child: Text(widget.miniQuedada.lugarInicio, textAlign: TextAlign.center,)
+                                    ),
+                                ],
+                            ),
+                        ),
+                    );
+                }
+            },
+        );
+    }
+
+    Future<ImageProvider> getImage() async{
+        String urlExistImage = "maps.googleapis.com";
+        String pathExistImage = "/maps/api/streetview/metadata";
+        String urlGetImage = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + widget.miniQuedada.latitud.toString()  + "," + widget.miniQuedada.longitud.toString() +  "&heading=151.78&pitch=-0.76&key=" + PLACES_API_KEY;
+
+        if ( await changeProfileImage(urlExistImage, pathExistImage))
+            return Image.network(urlGetImage).image;
         else {
-            Uint8List decoded = base64Decode(widget.miniQuedada.fotoLugar);
-            return Image.memory(decoded).image;
+            return AssetImage("assets/images/parkDog.jpg");
         }
     }
+
+    Future<bool> changeProfileImage(String url,String path) async {
+        var queryParameters = {
+            "size": "600x300",
+            "location": widget.miniQuedada.latitud.toString()  + "," + widget.miniQuedada.longitud.toString(),
+            "fov" : "90",
+            "heading" : "235",
+            "pitch" : "10",
+            "key" : PLACES_API_KEY,
+        };
+
+        Uri uri = new Uri.https(url, path,queryParameters);
+        var res;
+        try {
+            final response = await http.get(uri);
+             res = json.decode(response.body);
+             if (res.containsKey("status"))
+                 return res["status"] == "OK";
+        }catch(e) {
+            return false;
+        }
+    }
+
 }
+
