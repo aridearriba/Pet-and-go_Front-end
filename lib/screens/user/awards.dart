4dc971a27f3f310fcfd830e2892601407c9c464e
Ari de Arriba <aridease@gmail.com>
2020-05-27 22:55:16 +0200

    show avatars

--- lib/screens/user/awards.dart
+++ lib/screens/user/awards.dart
@@ -1,25 +1,14 @@
 import 'dart:convert';
-import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:petandgo/global/global.dart' as Global;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:petandgo/model/mascota.dart';
-import 'package:petandgo/screens/home.dart';
+import 'package:petandgo/model/avatar.dart';
 import 'package:petandgo/screens/menu/menu.dart';
-import 'package:petandgo/screens/pets/pet.dart';
-import 'package:petandgo/screens/user/edit.dart';
-import 'package:petandgo/screens/user/login.dart';
 import 'package:petandgo/model/user.dart';
-import 'package:petandgo/screens/pets/newPet.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:petandgo/screens/user/profile.dart';
-import 'package:petandgo/screens/user/sign-up.dart';
-
-import 'awards.dart';
 
 
 class Awards extends StatefulWidget {
@@ -32,6 +21,10 @@ class Awards extends StatefulWidget {
 
 class _AwardsState extends State<Awards>
 {
+    List<Avatar> _avatars;
+    final rows = <TableRow>[];
+
+
     nProfile() {
         Navigator.pushReplacement(
             context,
@@ -39,128 +32,108 @@ class _AwardsState extends State<Awards>
         );
     }
 
-
-    @override
     Widget build(BuildContext context) {
-        return Scaffold(
-            drawer: Menu(widget.user),
-            appBar: AppBar(
-                title: Text(
-                    'Premios',
-                    style: TextStyle(
-                        color: Colors.white,
-                    ),
-
-                ),
-                iconTheme: IconThemeData(
-                    color: Colors.white,
-                ),
-                actions: <Widget>[
-                    IconButton(
-                        icon: Icon(
-                            Icons.arrow_back,
+        return FutureBuilder<List<Avatar>>(
+            future: getAvatars(),
+            builder: (BuildContext context, AsyncSnapshot<List<Avatar>> snapshot) {
+            return Scaffold(
+                drawer: Menu(widget.user),
+                appBar: AppBar(
+                    title: Text(
+                        'Premios',
+                        style: TextStyle(
                             color: Colors.white,
                         ),
-                        onPressed: () => nProfile(),
+
                     ),
-                ],
-            ),
-            body: ListView(
+                    iconTheme: IconThemeData(
+                        color: Colors.white,
+                    ),
+                    actions: <Widget>[
+                        IconButton(
+                            icon: Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                            ),
+                            onPressed: () => nProfile(),
+                        ),
+                    ],
+                ),
+                body: snapshot.hasData ? _showAvatars() : _loadingAvatars()
+            );
+        });
+    }
+
+    Widget _loadingAvatars(){
+        return Center(
+            child: Column(
                 children: <Widget>[
                     Padding(
-                        padding: const EdgeInsets.only(
-                            top: 20.0, left: 30.0, right: 30.0),
-                        child: Column(
-                            children: <Widget>[
-                                CircleAvatar(
-                                    backgroundImage: Image.network('https://images.assetsdelivery.com/compings_v2/lar01joka/lar01joka1804/lar01joka180400005.jpg').image,
-                                    radius: 110,
-                                    backgroundColor: Colors.transparent,
-                                ),
-                                Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment
-                                        .start,
-                                    children: <Widget>[
-                                        // USER
-                                        Padding(
-                                            padding: const EdgeInsets.symmetric(vertical: 5.0),
-                                            child: Text(
-                                                "AVATAR",
-                                                style: TextStyle(
-                                                    color: Colors.black87,
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 18.0,
-                                                ),
-                                                textAlign: TextAlign.left,
-                                            )
-                                        ),
-                                        Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 5.0),
-                                            child: Table(
-                                                children: [
-                                                    TableRow(children: [
-                                                        _avatarBlocked(),
-                                                        _avatarBlocked(),
-                                                        _avatarBlocked(),
-                                                    ]),
-                                                    TableRow(children:[
-                                                        _avatarBlocked(),
-                                                        _avatarBlocked(),
-                                                        _avatarBlocked(),
-                                                    ]),
-                                                ])
-                                        ),
-                                    ],
-                                ),
-                                Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment
-                                        .start,
-                                    children: <Widget>[
-                                        // PUNTUACION
-                                        Padding(
-                                            padding: const EdgeInsets.only(top: 10, bottom: 5.0),
-                                            child: Text(
-                                                "COMPLEMENTOS",
-                                                style: TextStyle(
-                                                    color: Colors.black87,
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 18.0,
-                                                ),
-                                                textAlign: TextAlign.left,
-                                            )
-                                        ),
-                                        // nivel
-                                        Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 5.0),
-                                            child: Table(
-                                                children: [
-                                                    TableRow(children: [
-                                                        _complementBlocked(),
-                                                        _complementBlocked(),
-                                                        _complementBlocked(),
-                                                    ]),
-                                                    TableRow(children:[
-                                                        _complementBlocked(),
-                                                        _complementBlocked(),
-                                                        _complementBlocked(),
-                                                    ]),
-                                            ])
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        ),
+                        padding: EdgeInsets.all(20),
                     ),
-                ]
+                    SizedBox(
+                        width: 30.0,
+                        height: 30.0,
+
+                        child: CircularProgressIndicator(backgroundColor: Colors.green, valueColor: AlwaysStoppedAnimation(Colors.lightGreen)),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text('Cargando avatares...'),
+                    )
+                ],
             ),
         );
     }
 
-    Widget _avatarBlocked(){
+    Widget _showAvatars(){
+        return ListView(
+            children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20.0, left: 30.0, right: 30.0),
+                    child: Column(
+                        children: <Widget>[
+                            CircleAvatar(
+                                backgroundImage:
+                                    widget.user.avatar == null ?
+                                    Image.network(_avatars.first.image).image :
+                                    Image.network(widget.user.avatar).image,
+                                radius: 110,
+                                backgroundColor: Colors.transparent,
+                            ),
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                    // USER
+                                    Padding(
+                                        padding: const EdgeInsets.symmetric(vertical: 5.0),
+                                        child: Text(
+                                            "AVATAR",
+                                            style: TextStyle(
+                                                color: Colors.black87,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 18.0,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                        )
+                                    ),
+                                    Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 5.0),
+                                        child: Table(children: rows),
+                                    ),
+                                ],
+                            ),
+                        ],
+                    )
+                ),
+            ]
+        );
+    }
+
+    Widget _showAvatar(bool blocked, String link){
         return CircleAvatar(
             minRadius: 10,
             maxRadius: 40,
@@ -168,9 +141,9 @@ class _AwardsState extends State<Awards>
             child: Stack(
                 children: <Widget>[
                     ClipOval(
-                        child: Image.network('https://images.assetsdelivery.com/compings_v2/lar01joka/lar01joka1804/lar01joka180400005.jpg'),
+                        child: Image.network(link),
                     ),
-                    Positioned(
+                    blocked ? Positioned(
                         top: 0,
                         left: 60,
                         child: Container(
@@ -181,36 +154,63 @@ class _AwardsState extends State<Awards>
                                 child: Icon(Icons.lock, color: Colors.black, size: 15),
                             )
                         )
+                    ) : Positioned(
+                            top: 0,
+                            left: 60,
+                            child: Container(
+                                width: 20,
+                                height: 20,)
                     ),
                 ]
             ),
         );
     }
 
-    Widget _complementBlocked(){
-        return CircleAvatar(
-            minRadius: 10,
-            maxRadius: 40,
-            backgroundColor: Colors.transparent,
-            child: Stack(
-                children: <Widget>[
-                    ClipOval(
-                        child: Image.network('https://i.dlpng.com/static/png/6711347_preview.png'),
-                    ),
-                    Positioned(
-                        top: 0,
-                        left: 60,
-                        child: Container(
-                            width: 20,
-                            height: 20,
-                            child:
-                            ClipOval(
-                                child: Icon(Icons.lock, color: Colors.black, size: 15),
-                            ),
-                        )
-                    ),
-                ]
-            ),
-        );
+    Future<List<Avatar>> getAvatars() async{
+        var email = widget.user.email;
+        final response = await http.get(new Uri.http(Global.apiURL, "/api/avatares/"));
+        Iterable list = json.decode(response.body);
+        _avatars = list.map((model) =>  Avatar.fromJson(model)).toList();
+
+        _avatars.sort((a,b) => a.level.compareTo(b.level));
+
+        rows.clear();
+        int count = 3;
+        Avatar avatar1, avatar2, avatar3;
+        for (var avatar in _avatars) {
+            if (count == 1) {
+                avatar3 = avatar;
+                count = 4;
+                _createRow(avatar1, avatar2, avatar3);
+            }
+            if (count == 2) avatar2 = avatar;
+            if (count == 3) avatar1 = avatar;
+
+            count--;
+        }
+
+        if (count == 1) {
+            avatar3 = new Avatar();
+            avatar3.image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ursulamascaro.es%2Fblank-product.html&psig=AOvVaw0Z-SGnU2Ui9HWuXEJHzRkI&ust=1590695673220000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjY_Kbp1OkCFQAAAAAdAAAAABAS";
+            _createRow(avatar1, avatar2, avatar3);
+        }
+
+        if (count == 2) {
+            avatar3 = new Avatar();
+            avatar3.image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ursulamascaro.es%2Fblank-product.html&psig=AOvVaw0Z-SGnU2Ui9HWuXEJHzRkI&ust=1590695673220000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjY_Kbp1OkCFQAAAAAdAAAAABAS";
+            avatar2 = new Avatar();
+            avatar2.image = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ursulamascaro.es%2Fblank-product.html&psig=AOvVaw0Z-SGnU2Ui9HWuXEJHzRkI&ust=1590695673220000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjY_Kbp1OkCFQAAAAAdAAAAABAS";
+            _createRow(avatar1, avatar2, avatar3);
+        }
+
+        return _avatars;
+    }
+
+    TableRow _createRow(Avatar avatar1, Avatar avatar2, Avatar avatar3) {
+        rows.add(TableRow(children: [
+            _showAvatar(widget.user.level <= avatar1.level ? true : false, avatar1.image),
+            _showAvatar(widget.user.level <= avatar2.level ? true : false, avatar2.image),
+            _showAvatar(widget.user.level <= avatar3.level ? true : false, avatar3.image)
+        ]));
     }
 }
